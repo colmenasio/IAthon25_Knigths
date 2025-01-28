5e81ed31cd4cddbc651c789b8873ca3cc1469c3b
colmenasio <a.colmenero.fernandez@gmail.com>
2025-01-28 17:32:57 +0100

    [Ref] Moved markdown converted and updated it

--- src/backend/conversion/mrk_to_html.py
+++ src/backend/conversion/mrk_to_html.py
@@ -1,7 +1,7 @@
 import markdown
 from backend.gpt_utils.gpt_analysis_env import GptAnalysisEnv
 
-def generatehtml(analysis_env: GptAnalysisEnv):
+def generate_html(analysis_env: GptAnalysisEnv, output_path: str) -> None:
     # Contenido en formato Markdown
     markdown_text = analysis_env.summarize_conversation()
 
@@ -9,5 +9,5 @@ def generatehtml(analysis_env: GptAnalysisEnv):
     html_output = markdown.markdown(markdown_text)
 
     # Guardar el HTML resultante en un archivo
-    with open(" ", "w", encoding="utf-8") as file:
+    with open(output_path, "w", encoding="utf-8") as file:
         file.write(html_output)
